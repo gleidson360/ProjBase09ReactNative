6951093a4e514c78ef5b0309486d6f63d5a736f6
Gleidson Medrado <gleidsonmedrado@hotmail.com>
2024-03-22 13:39:07 -0300

    Importando componentes

--- App.js
+++ App.js
@@ -24,6 +24,67 @@ export default function App() {
 
   return (
     <ScrollView>
+      <StatusBar barStyle="dark-content" backgroundColor="#fff"/>
+
+      <Grafico porcentagem={ (TotalGastos() / limite ) * 100}/>
+
+      <Janela>
+        <Text style={{ fontSize: 18, marginBottom: 16, textAlign: "center" }}> 
+          Definir Limites 
+        </Text>
+
+        <TextInput
+          style={{ backgroundColor: "#fff", padding: 8 }}
+          value={ limite }
+          onChangeText={ definirLimite }
+          placeholder="Limite (R$)"
+          keyboardType="number-pad"
+        />
+      </Janela>
+
+      <Janela>
+        <Text style={{ fontSize: 18, marginBottom: 16, textAlign: "center" }}> 
+          Adicionar Gastos 
+        </Text>
+
+        <TextInput
+          style={{ backgroundColor: "#fff", marginBottom: 16, padding: 8 }}
+          value={ categoria }
+          onChangeText={ mudarCategoria }
+          placeholder="Gasto"
+          keyboardType="default"
+        />
+
+        <TextInput
+          style={{ backgroundColor: "#fff", marginBottom: 16, padding: 8 }}
+          value={ valor }
+          onChangeText={ mudarValor }
+          placeholder="Valor (R$)"
+          keyboardType="number-pad"
+        />
+
+        <Pressable onPress={ AdicionarGastos }>
+          <View style={{ backgroundColor: "#698269" }}>
+            <Text style={{ color: "#fff", paddingVertical: 16, textAlign: "center" }}> 
+              Adicionar 
+            </Text>
+          </View>
+        </Pressable>
+      </Janela>
+
+      <Janela>
+        <Text style={{ fontSize: 18, marginBottom: 16, textAlign: "center" }}>
+          Histórico de Gastos
+        </Text>
+
+        {
+          gastos.map(function(gasto, indice) {
+            return <Text key={ indice } style={{ color: "#698269" }}> 
+              {gasto.categoria} - R$ {gasto.valor}
+            </Text>
+          })
+        }
+      </Janela>
 
     </ScrollView>
   );
