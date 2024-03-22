6b74ace1761c2b3a476f9a31578f2be77e06d891
Gleidson Medrado <gleidsonmedrado@hotmail.com>
2024-03-22 11:31:58 -0300

    Criando gráfico

---
+++ src/components/Grafico.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./../styles/Grafico";
+
+export default function Grafico(props) {
+    return <View style={ styles.grafico }>
+        <View
+            style={ [styles.graficoBarra , { width: `${props.porcentagem}%`} ]}
+        >
+            <Text style={ styles.graficoTexto }>
+                { props.porcentagem + "%" }
+            </Text>
+        </View>
+    </View>
+}
---
+++ src/styles/Grafico.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    grafico: {
+        backgroundColor: "#eee",
+        margin: 16,
+    },
+    graficoBarra: {
+        backgroundColor: "#698269",  
+    },
+    graficoTexto: {
+        color: "#fff",   
+        paddingVertical: 16,   
+        textAlign: "center",   
+    }
+})
